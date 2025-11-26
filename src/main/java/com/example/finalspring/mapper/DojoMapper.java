55d6183a88c1a0af09e42ae3605dc45df936e009
Sandibala <sandibalaamze17@gmail.com>
2025-11-26 19:38:32 +0500

    feat: Add base mapper interfaces from feature/dto

--- src/main/java/com/example/finalspring/mapper/DojoMapper.java
+++ src/main/java/com/example/finalspring/mapper/DojoMapper.java
@@ -3,13 +3,16 @@ package com.example.finalspring.mapper;
 import com.example.finalspring.dto.DojoDto;
 import com.example.finalspring.entity.Dojo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DojoMapper {
-
+    @Mapping(target = "nameDto", source = "name")
     DojoDto toDto(Dojo dojo);
+    @Mapping(target = "name", source = "nameDto")
     Dojo toEntity(DojoDto dojoDto);
     List<DojoDto> toDtoList(List<Dojo> dojoList);
+    List<Dojo> toEntityList(List<DojoDto> dojoDtoList);
 }
--- src/main/java/com/example/finalspring/mapper/KatanaMapper.java
+++ src/main/java/com/example/finalspring/mapper/KatanaMapper.java
@@ -2,14 +2,22 @@ package com.example.finalspring.mapper;
 
 import com.example.finalspring.dto.KatanaDto;
 import com.example.finalspring.entity.Katana;
+import com.example.finalspring.entity.Samurai;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",uses = SamuraiMapper.class)
 public interface KatanaMapper {
-
+    @Mapping(target = "nameDto", source = "name")
+    @Mapping(target = "bySwordsmanDto", source = "bySwordsman")
+    @Mapping(target = "samuraiDto", source = " samurai")
     KatanaDto toDto(Katana katana);
+    @Mapping(target = "name", source = "nameDto")
+    @Mapping(target = "bySwordsman", source = "bySwordsmanDto")
+    @Mapping(target = "samurai", source = "samuraiDto")
     Katana toEntity(KatanaDto katanaDto);
     List<KatanaDto> toDtoList(List<Katana> katanaList);
+    List<Katana> toEntityList(List<KatanaDto> katanaDtoList);//na zapas
 }
